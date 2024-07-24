fab8add3c7058e00b955eabb9cd5f36826aa7648
Cao Khai <goku1331999@gmail.com>
2024-07-24 16:07:24 +0700

    latest update

--- backup.bat
+++ backup.bat
@@ -1,7 +1,7 @@
 @echo off
 xcopy C:\"Users\klove\Desktop\Temporary folder\C++ Practice\First Window app" E:\"First Window app" /Y /S
-REM xcopy C:\"Users\klove\Downloads\SFML" E:\"SFML" /Y /S
-REM xcopy C:\"Users\klove\Downloads\SDL2" E:\"SDL2" /Y /S
-xcopy C:\"Users\klove\Documents\repos" E:\"repos\" /Y /S
+REM xcopy C:\"Users\klove\Downloads\SFML" E:\"SFML" /D /Y /S
+REM xcopy C:\"Users\klove\Downloads\SDL2" E:\"SDL2" /D /Y /S
+xcopy C:\"Users\klove\Documents\repos" E:\"repos\" /D /Y /S
 xcopy C:\Users\klove\AppData\Roaming\.ema* E:\"First Window app" /Y
 sh.exe ./backupOnline.sh
